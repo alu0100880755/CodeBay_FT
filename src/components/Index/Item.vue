e0a08b2f8d2ec1ff62baa7515b1ffca75d2cd7a5
Iván Josué Borges González <ivanbor8@gmail.com>
2020-10-03 16:56:22 +0100

    Items

---
+++ src/components/Index/Item.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 text-left">
+  <q-card class="my-card">
+
+    <q-card-section vertical>
+      <q-img
+      :src="item_image"
+      class="col-3 col-sm-3 item_image"
+      spinner-color="white"
+      ratio="4/3"
+      height="200px"
+    />
+    </q-card-section>
+
+      <q-card-section vertical>
+        <div class="text-h6">{{item_name}}</div>
+        <div class="text-subtitle2">{{item_category}}</div>
+      </q-card-section>
+
+      <q-card-section vertical class="q-pt-none">
+        {{ item_price }}
+      </q-card-section>
+
+    </q-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  data () {
+    return {
+    }
+  },
+  // Child props for card
+  props: {
+    item_image: {
+      type: String,
+      required: true
+    },
+    item_name: {
+      type: String,
+      required: true
+    },
+    item_category: {
+      type: String,
+      required: true
+    },
+    item_price: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.my-card {
+  /* font-size: calc(5px + 6 * ((100vw - 20px) / 680)); */
+  overflow: auto;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,60 @@
 <template>
-  <!-- <q-page class="flex flex-center"> -->
+  <div class="Page_content">
+    <!-- <q-page class="flex flex-center"> -->
 
-    <!-- <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    > -->
-<!--  -->
-<Carousel></Carousel>
+    <Carousel></Carousel>
+    <div class="row">
+    <Item
+      v-for="item in available_items"
+      :key="item.item_name"
+      v-bind="item"
+    />
 
-  <!-- </q-page> -->
+    </div>
+
+    <!-- </q-page> -->
+  </div>
 </template>
 
 <script>
 import Carousel from '../components/Index/Carousel'
+import Item from '../components/Index/Item'
 
 export default {
   name: 'PageIndex',
   components: {
-    Carousel
+    Carousel,
+    Item
+  },
+  data () {
+    return {
+      available_items: [
+        {
+          item_image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRFROUOGAt6R58jU0JUVDpN7WfEEClI78b4ZozLHZYbnRnNueVPF2U&usqp=CAc',
+          item_name: 'Samsung Note 20',
+          item_category: 'Mobile',
+          item_price: '1200 $'
+        },
+        {
+          item_image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSiaq-L2RIIZmZQJZ1fn6wydkO3W_wElzStKFsX8lAFJfliC59reA8&usqp=CAc',
+          item_name: 'Apple Iphone 11',
+          item_category: 'Mobile',
+          item_price: '1300 $'
+        },
+        {
+          item_image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ4r1Y5QHUWNC1v03hdMLNotfPBWSluTYICfPUKzGHLILTf58LKuq8&usqp=CAc',
+          item_name: 'Xiaomi Mi 10',
+          item_category: 'Mobile',
+          item_price: '1200 $'
+        },
+        {
+          item_image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRPfXGd1o0kSNyLnqiFMNcMKD9_gbpGZj1GGcxeJNbfHIHo8DxgPmQ&usqp=CAc',
+          item_name: 'Oneplus 8',
+          item_category: 'Mobile',
+          item_price: '1200 $'
+        }
+      ]
+    }
   }
 }
-
 </script>
