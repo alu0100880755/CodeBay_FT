d55e6f66333e4612fd38b61fb1a1294a7a1f0bf8
Iván Josué Borges González <ivanbor8@gmail.com>
2020-10-05 22:40:02 +0100

    Items style fix

--- src/components/Index/Item.vue
+++ src/components/Index/Item.vue
@@ -3,22 +3,25 @@
   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 text-left">
   <q-card class="my-card">
 
+    <!-- Item image section -->
     <q-card-section vertical>
       <q-img
       :src="item_image"
       class="col-3 col-sm-3 item_image"
       spinner-color="white"
       ratio="4/3"
-      height="200px"
+      :height="height_device + unit"
     />
     </q-card-section>
 
-      <q-card-section vertical>
+      <!-- Item name section -->
+      <q-card-section vertical class="item_name">
         <div class="text-h6">{{item_name}}</div>
         <div class="text-subtitle2">{{item_category}}</div>
       </q-card-section>
 
-      <q-card-section vertical class="q-pt-none">
+      <!-- Item price section -->
+      <q-card-section vertical class="item_price q-pt-none">
         {{ item_price }} {{ $t('coin') }}
       </q-card-section>
 
@@ -32,6 +35,8 @@ export default {
   name: 'Item',
   data () {
     return {
+      height_device: 55,
+      unit: 'vh'
     }
   },
   // Child props for card
@@ -52,13 +57,31 @@ export default {
       type: Number,
       required: true
     }
+  },
+  mounted () {
+    if (this.$q.platform.is.mobile) {
+      this.height_device = 25
+    }
   }
 }
 </script>
 
 <style>
 .my-card {
-  /* font-size: calc(5px + 6 * ((100vw - 20px) / 680)); */
   overflow: auto;
+  margin-left: 6%;
+  margin-right: 6%;
+  margin-top: 6%;
+  margin-bottom: 6%;
+}
+
+.item_image {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+  max-width: 100%;
+  max-height: 100%;
 }
 </style>
